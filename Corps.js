b663c5a6ad9274b3c208d99c17b7b79e5e452668
pad <pad@maitrebitcoin.com>
2021-01-11 08:24:27 +0100

    point adding ok

--- Corps.js
+++ Corps.js
@@ -5,7 +5,7 @@ function Corps( N ) {
     console.assert( typeof  N == 'bigint' )
     this.N      = N
     // optim for inversion
-    this.NMinus1 = this.sub(  N, BigInt(1) )
+    this.NMinus2 = this.sub(  N, BigInt(2) )
 }
 
 // Addition modulo N
@@ -14,24 +14,39 @@ Corps.prototype.add = function ( x,y )  {
 }
 // Substraction modulo N
 Corps.prototype.sub = function ( x,y )  {
-     return ( x - y ) % this.N;
+     var res = ( x - y ) % this.N;
+     // the result must be positive
+     if (res<0) 
+         res += this.N;
+    return res
 }
 
 // Multiplication modulo N
 Corps.prototype.mult = function ( x,y )  {
     return ( x * y ) % this.N;
 }
+// square modulo N : x^2
+Corps.prototype.square = function ( x )  {
+    return ( x * x ) % this.N;
+}
+// cube modulo N : x^3
+Corps.prototype.cube = function ( x )  {
+    return ( x * x * x ) % this.N;
+}
 
 // exponentiation  : x ^ y
 Corps.prototype.exp = function ( x,y )  {
      var r = BigInt(1)
+     var xPow2N = x
      while (y>0) {
-        r = mult( r, r)
+      
         // if bit 0 is set
-        if (y % 1 == 1) {
-            r = mult( r, x)
+        var bit1 = y % BigInt(2)
+        if (bit1 == BigInt(1)) {
+            r = this.mult( r, xPow2N)
         }
-        y = y / 2
+        y = y / BigInt(2)
+        xPow2N = this.mult( xPow2N, xPow2N)
      }
      return r;
 }
@@ -41,5 +56,7 @@ Corps.prototype.exp = function ( x,y )  {
 Corps.prototype.inversion = function ( x )  {
     console.assert( x != 0 );
     // euler formula
-    return exp( x, this.NMinus1 )
+    var invx = this.exp( x, this.NMinus2 );
+    console.assert( this.mult(x, invx) == BigInt(1) )
+    return invx  ;
 }
--- EllipticCurve.js
+++ EllipticCurve.js
@@ -5,6 +5,11 @@ function ECPoint(x,y) {
     this.x = x;
     this.y = y;
 }
+// check if 2 points are equals 
+ECPoint.prototype.equal = function ( pointA, pointB )  {
+    return    pointA.x == pointB.x 
+           && pointA.y == pointB.y;
+}
 
 // constructor
 function EllipticCurveSecp256k1(  ) {
@@ -21,21 +26,66 @@ function EllipticCurveSecp256k1(  ) {
     this.N = BigInt("0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F");
     this.Corps = new Corps( this.N );
   
+    console.assert( this.pointOnCurve(this.G) );  
+}
+
+// check if a point is on the curve
+//  y^{2}=x^{3}+ax+b
+EllipticCurveSecp256k1.prototype.pointOnCurve = function ( pointA )  {
+    var Y2 =  this.Corps.square( pointA.y );
+    var X3 =  this.Corps.cube(   pointA.x );
+    // simplication furmula a == 0
+    return Y2 ==  this.Corps.add(X3,  this.b );
 }
 
 // Point doubking
-EllipticCurveSecp256k1.prototype.pointDoubling = function ( point )  {
-     r = (a + b ) % this.N;
-     return r;
+EllipticCurveSecp256k1.prototype.pointDoubling = function ( pointA )  {
+    // formulas can be found here: 
+    // https://en.wikipedia.org/wiki/Elliptic_curve_point_multiplication
+
+    // lambda = (3* A.x^2 + a ) / (2 * A.y )
+    var lambdaN      = this.Corps.square( pointA.x );
+        lambdaN      = this.Corps.mult( BigInt(3), lambdaN );
+    var lambdaD      = this.Corps.mult( BigInt(2), pointA.y );
+    var invlambdaD   = this.Corps.inversion( lambdaD );
+    var lambda       = this.Corps.mult(lambdaN, invlambdaD ); 
+
+    // rx = L^2 - A.x - A.x
+    var lambdaSquare =  this.Corps.square(lambda)
+    var rx           =  this.Corps.sub(lambdaSquare, pointA.x)
+        rx           =  this.Corps.sub(rx, pointA.x)
+    // ry = L*( A.X - rx ) - A.Y
+    var ry           =  this.Corps.mult(lambda, this.Corps.sub(pointA.x, rx) )
+        ry           =  this.Corps.sub( ry, pointA.y )
+
+    var pointRes = new ECPoint( rx, ry);    
+    console.assert( this.pointOnCurve(pointRes) );
+    return pointRes;
 }
 
 // Point Addition
 EllipticCurveSecp256k1.prototype.pointAdding = function ( pointA, pointB )  {
-    // lambda = (B.y - A.Y ) / (B.x -A.x )
-    var lambdaN    = this.Corps.sub(pointB.y, pointA.y );
-    var lambdaD    = this.Corps.sub(pointB.x, pointA.x );
-    var invlambdaD = this.Corps.inversion( lambdaD );
-    var lambda     = this.Corps.mult(lambdaN, invlambdaD );
+    // if A == B, spécial case
+    if (pointA.equal(pointB))
+        return this.pointDoubling(pointA, pointB);
 
-    return r;
+    // formulas can be found here: 
+    // https://en.wikipedia.org/wiki/Elliptic_curve_point_multiplication
+
+    // lambda = (B.y - A.Y ) / (B.x -A.x )
+    var lambdaN      = this.Corps.sub(pointB.y, pointA.y );
+    var lambdaD      = this.Corps.sub(pointB.x, pointA.x );
+    var invlambdaD   = this.Corps.inversion( lambdaD );
+    var lambda       = this.Corps.mult(lambdaN, invlambdaD );
+    // rx = L^2 - A.x - B.x
+    var lambdaSquare =  this.Corps.square(lambda)
+    var rx           =  this.Corps.sub(lambdaSquare, pointA.x)
+        rx           =  this.Corps.sub(rx, pointB.x)
+    // ry = L*( A.X - rx ) - A.Y
+    var ry           =  this.Corps.mult(lambda, this.Corps.sub(pointA.x, rx) )
+        ry           =  this.Corps.sub( ry, pointA.y )
+
+    var pointRes = ECPoint( rx, ry);
+    console.assert( this.pointOnCurve(pointRes) );    
+    return pointRes;    
 }
