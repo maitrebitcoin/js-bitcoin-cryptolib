c8fc3ee79720e372a012c83b63fbe351a4614545
pad <pad@maitrebitcoin.com>
2021-04-03 14:11:13 +0200

    more debug for IOS

--- sample/sample-utils.js
+++ sample/sample-utils.js
@@ -15,20 +15,33 @@
  */
 function onCopy(elementId) {
     var element = document.getElementById( elementId )
+    // dato to copy into de clipboard
     var data    = element.innerHTML
+
+    // hack for IOS
+    var elementEdit = document.createElement("input")
+    elementEdit.contentEditable = true;
+    elementEdit.readOnly  = false;
+    elementEdit.value = data      
+    document.body.appendChild(elementEdit)
 
     var range = document.createRange();
     // select the element
-    range.selectNode(element);  
-    window.getSelection().addRange(range);  
+    range.selectNodeContents(elementEdit);  
+    elementEdit.setSelectionRange(0, 999999);
+    
+    var s = window.getSelection();
+    s.removeAllRanges();
+    s.addRange(range);
   
     try {  
-      // Now that we've selected the anchor text, execute the copy command  
+      //  copy to clipboard
       var successful = document.execCommand('copy', false, null);
-      var msg = successful ? 'successful' : 'unsuccessful'; 
-  
-      if(true){
-        alert("public adress :\n\n" + data + "\n\nsucessfully copied to the clipboard"  )
+      if(successful){
+         alert("public adress :\n\n" + data + "\n\nsucessfully copied to the clipboard"  )
+      }
+      else {
+         alert("copy to the clipboard failed\n" )
       }
   
     } catch(err) {  
