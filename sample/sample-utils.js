07fe0b17fc6a2475fd58bffe3bd9f637d0c0eeee
pad <pad@maitrebitcoin.com>
2021-04-03 13:50:29 +0200

    sample new copy code in IOS

--- sample/sample-utils.js
+++ sample/sample-utils.js
@@ -1,25 +1,47 @@
-
 /**
- *  copy the content of <elementId> to the clipboard
+ ****************************************************** 
+ * @file    sample-utils.js
+ * @file    common functions= for the samples
+ * @author  pad@maitrebitcoin.com
+ * @module  js-bitcoin-criptolib
+ * 
+ * @license LGPL-3.0 
+ ******************************************************
  */
-function onCopy( elementId ) {
-    var data =  document.getElementById( elementId ).innerHTML
-    // can we write into the clipboard ?
-    navigator.permissions.query({name: "clipboard-write"}).then(result => {
-        if (result.state == "granted" || result.state == "prompt") {
-            navigator.clipboard.writeText(data)
-            alert("public adress :\n\n" + data + "\n\nsucessfully copied to the clipboard"  )
-        }
-        else {
-            alert("acces to the clipboard denied\n"  + result.state  )
-        }
-    });
-
-}   
-
 
 /**
- *  display an error in the page
+ * copy the content of an element into the clipboard - IOS conpatible version
+ * @param {string} elementId ID of the dom element to be copied
+ */
+function onCopy(elementId) {
+    var element = document.getElementById( elementId )
+    var data    = element.innerHTML
+
+    var range = document.createRange();
+    // select the element
+    range.selectNode(element);  
+    window.getSelection().addRange(range);  
+  
+    try {  
+      // Now that we've selected the anchor text, execute the copy command  
+      var successful = document.execCommand('copy', false, null);
+      var msg = successful ? 'successful' : 'unsuccessful'; 
+  
+      if(true){
+        alert("public adress :\n\n" + data + "\n\nsucessfully copied to the clipboard"  )
+      }
+  
+    } catch(err) {  
+        alert("copy to the clipboard failed\n"  + err.message  )
+    }  
+      // Remove the selections 
+    window.getSelection().removeAllRanges();
+}
+
+/**
+ *  shwn an error in the html page
+ * @param {Error} error the error to be displayed
+ * @param {string} elementId ID if the dom element where to show the error
  */
 function showError( error, elementId ) {
     if (!elementId)
